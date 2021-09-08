60670a449f79aa40cce5343835e785729e0028ec
Arthur Feng <kuanchungfeng@gmail.com>
2021-09-08 21:43:45 +0800

    想要吃魚

---
+++ src/domain/Fish.java
@@ -0,0 +1,11 @@
+package domain;
+
+/**
+ * @author Arthur on 2021/9/8
+ */
+public class Fish extends Meat{
+
+    public Fish(String name) {
+        super(name);
+    }
+}
--- src/simpleFactory/SimpleMeatFactory.java
+++ src/simpleFactory/SimpleMeatFactory.java
@@ -1,9 +1,6 @@
 package simpleFactory;
 
-import domain.Beef;
-import domain.Chicken;
-import domain.Meat;
-import domain.Pork;
+import domain.*;
 
 /**
  * @author Arthur on 2021/9/8
@@ -15,6 +12,8 @@ public class SimpleMeatFactory {
             return new Beef("和牛");
         }else if(meatType.equals("雞肉")){
             return new Chicken("土雞");
+        }else if(meatType.equals("魚肉")){
+            return new Fish("鯖魚");
         }else {
             return new Pork("伊比利豬");
         }
